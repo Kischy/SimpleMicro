30f93687941e97cb48e3224d15dae844a4922468
Christian Kischy Haege <kischy@gmx.de>
2021-04-21 21:29:49 +0200

    Started Linked List

--- LList.h
+++ LList.h
@@ -2,20 +2,40 @@
 #define LLIST_H
 
 
-template<class T>
+template<class value_type>
 class LList
 {
 private:
-    template<class T> 
     struct LListElement
     {
-        T value;
+        value_type value;
         LListElement* nextElement = nullptr;
     };
 
+    size_t elementCount = 0;
+    LListElement* firstElement = nullptr;
 
-public: 
+public:
+    bool empty() const;
+    size_t size() const;
+
+
+
 };
+
+
+template<class value_type>
+bool LList<value_type>::empty() const
+{
+    return size == 0;
+}
+
+template<class value_type>
+size_t LList<value_type>::size() const
+{
+    return elementCount;
+}
+
 
 
 
